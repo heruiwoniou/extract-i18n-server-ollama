cc1f2a8da46768c9a6f0e61f53be2decf4735ec1
heruiwoniou <heruiwoniou@qq.com>
2024-08-22 07:58:52 +0000

    trim the tranlated content

--- src/index.mjs
+++ src/index.mjs
@@ -26,7 +26,7 @@ app.get("/translate", async (req, res) => {
     ],
   });
 
-  const translated = response.message.content.trim();
+  const translated = response.message.content.replace(/(^\s*)|(\s*$)/, '');
   console.log(`Translated: ${response.message.content}`);
 
   if (translated === "error") {
